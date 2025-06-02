7e846c8180ab3a537fe34a7c6465a2918fcd7526
ryanohmm <oboyleryan1@gmail.com>
2025-06-02 13:23:51 -0400

    updating js

---
+++ summer-chores/callbackVersion.js
@@ -0,0 +1,70 @@
+function mowYard(personName, callback) {
+    setTimeout(() => {
+        console.log(`${personName} mowed the yard.`);
+        callback();
+    }, 2000); // Simulating the time taken to mow the yard
+}
+
+// Example usage:
+mowYard('Ryan', () => {
+    console.log('Chore complete!');
+});
+
+function weedEat(personName, callback) {
+    setTimeout(() => {
+        if (Math.random() > 0.2) { 
+            console.log(`${personName} finished using the weed eater.`);
+            callback();
+        } else {
+            console.log(`${personName} fell asleep after mowing the yard.`);
+        }
+    }, 1500);
+}
+function trimHedges(personName, callback) {
+    setTimeout(() => {
+        if (Math.random() > 0.4) {
+        console.log(`${personName} finished trimming the hedges`);
+        callback();
+        } else {
+            console.log(`${personName} fell asleep after mowing the yard.`);
+        }
+    }, 2500);
+}
+
+function collectWood(personName, callback) {
+    setTimeout(() => {
+        if (Math.random() > 0.1) {
+            console.log(`${personName} finished collecting wood.`);
+            callback();
+        } else {
+            console.log(`${personName} fell asleep after trimming the hedges.`);
+        }
+    }, 1750);
+}
+function waterGarden(personName, callback) {
+    setTimeout(() => {
+        if (Math.random() > 0.25) {
+            console.log(`${personName} finished watering the garden.`);
+            callback();
+        } else {
+            console.log(`${personName} fell asleep after collecting wood.`);
+        }
+    }, 500);
+}
+
+function doSummerChores(personName) {
+    mowYard(personName, () => {
+        weedEat(personName, () => {
+            trimHedges(personName, () => {
+                collectWood(personName, () => {
+                    waterGarden(personName, () => {
+                        console.log(`${personName} finished all their chores!`);
+                    });
+                });
+            });
+        });
+    });
+}
+
+
+doSummerChores('Ryan');
